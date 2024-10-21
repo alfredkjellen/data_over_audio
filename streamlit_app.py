954a999797465fad4926b3743821826b595aa558
alfredkjellen <81372499+alfredkjellen@users.noreply.github.com>
2024-10-21 09:51:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,34 @@
 import streamlit as st
 from pathlib import Path
 from translate import binary_to_text, file_to_binary
-from demodulate import find_pilot_signal, decode_fsk_signal, PILOT_CHUNK_SIZE, FREQUENCIES, MAGNITUDE_THRESHOLDS
-from plots import highlight_binary_data, plot_frequency_domain, plot_magnitude_over_time, plot_time_domain
+from demodulate import get_pilot_start, decode_fsk_signal, FREQUENCIES, MAGNITUDE_THRESHOLDS, BIT_DURATION, PILOT_DURATION
+from plots import get_highlighted_bits, plot_frequency_domain, plot_magnitude_over_time, plot_time_domain
 
 if __name__ == "__main__":
-    audio_folder = Path("signal")
+    audio_folder = Path("8f")
 
     st.title("Data Over Audio Demodulation")
     audio_files = list(audio_folder.glob("*.wav"))
-    actual_binary = file_to_binary("text.txt")
+    correct_bits = file_to_binary("text.txt")
 
     selected_audio = st.selectbox("Select an audio file", audio_files)
 
     if selected_audio:
-        binary_data, audio_data, sample_rate = decode_fsk_signal(selected_audio)
-        if binary_data:
+        bits, audio_data, sample_rate = decode_fsk_signal(selected_audio)
+        if bits:
             
-            binary_data_str = ''.join(map(str, binary_data))
-            highlighted_binary_data = highlight_binary_data(binary_data_str, actual_binary)
-            st.markdown(f"Decoded binary data: {highlighted_binary_data}", unsafe_allow_html=True)
+            bits_str = ''.join(map(str, bits))
+            highlighted_bits = get_highlighted_bits(bits_str, correct_bits)
+            st.markdown(f"Decoded binary data: {highlighted_bits}", unsafe_allow_html=True)
 
-            decoded_text = binary_to_text(binary_data)
+            decoded_text = binary_to_text(bits)
             st.write(decoded_text)
             
-            pilot_start = int(find_pilot_signal(audio_data))
-            start_pos = pilot_start + PILOT_CHUNK_SIZE
+            pilot_start = int(get_pilot_start(audio_data))
+            pilot_chunk_size = int(PILOT_DURATION * sample_rate)
+            start_pos = pilot_start + pilot_chunk_size
 
-            st.plotly_chart(plot_time_domain(audio_data, sample_rate, binary_data, start_pos, actual_binary))
+            st.plotly_chart(plot_time_domain(audio_data, sample_rate, BIT_DURATION, bits, start_pos, correct_bits))
             st.plotly_chart(plot_frequency_domain(audio_data, sample_rate))
             
             for freq in FREQUENCIES:
